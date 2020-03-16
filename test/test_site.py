c8aff9d185301e9e935ed81c61feff9646d4fcc2
Kostis Anagnostopoulos <ankostis@gmail.com>
2020-03-16 21:13:18 +0200

    enh(site.TC): build site from pytest py-interpreter ...
    to control logging and view pytestfull stack-traces on error.

--- test/test_site.py
+++ test/test_site.py
@@ -1,9 +1,11 @@
 # Copyright 2016, Yahoo Inc.
 # Licensed under the terms of the Apache License, Version 2.0. See the LICENSE file associated with the project for terms.
+import importlib
+import logging
+import os
 import os.path as osp
 import subprocess
 import sys
-
 
 proj_path = osp.join(osp.dirname(__file__), "..")
 
@@ -30,6 +32,12 @@ def test_README_as_PyPi_landing_page(monkeypatch):
     )
 
 
-def test_sphinx_html():
+def test_sphinx_html(monkeypatch):
     # Fail on warnings, but don't rebuild all files (no `-a`),
-    subprocess.check_output("python setup.py build_sphinx -W".split(), cwd=proj_path)
+    # rm -r ./build/sphinx/ # to fully test site.
+    site_args = list("setup.py build_sphinx -W".split())
+    if logging.getLogger(__name__).isEnabledFor(logging.INFO):
+        site_args.append("-v")
+    monkeypatch.setattr(sys, "argv", site_args)
+    monkeypatch.chdir(proj_path)
+    importlib.import_module("setup")
