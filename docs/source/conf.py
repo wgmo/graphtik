ef462d073df312197ffb2d22b6f0eed473ebfdcf
Kostis Anagnostopoulos <ankostis@gmail.com>
2020-04-27 15:13:44 +0300

    ENH(SITE): CSS for Colored Roles in <s5defs>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -168,6 +168,7 @@ def linkcode_resolve(domain, info):
 
 
 rst_epilog = """
+
 .. include:: <s5defs.txt>
 .. _Graphkit: https://github.com/yahoo/graphkit
 .. _Graphviz: https://graphviz.org
@@ -220,7 +221,8 @@ html_theme_options = {}
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
+html_static_path = ["_static"]
+html_css_files = ["s5defs.css"]
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
