ec7872daa4ffdd36bc3cfb52ad81e3af22dad296
Kostis Anagnostopoulos <ankostis@gmail.com>
2020-05-20 18:23:57 +0300

    doc(base.TC): fix attribution in copyright notice

--- test/test_base.py
+++ test/test_base.py
@@ -1,4 +1,4 @@
-# Copyright 2016, Yahoo Inc.
+# Copyright 2020-2020, Kostis Anagnostopoulos;
 # Licensed under the terms of the Apache License, Version 2.0. See the LICENSE file associated with the project for terms.
 import functools as fnt
 import itertools as itt
